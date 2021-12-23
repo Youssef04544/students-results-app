9f4ee4e4a4c07ba69b7fd7204dfdc2719b8ac146
Youssef04544 <youssefbenyedder04@gmail.com>
2021-12-23 12:25:05 +0100

    Added row styling depending on the scores

--- src/components/StudentRow.jsx
+++ src/components/StudentRow.jsx
@@ -3,25 +3,24 @@ const StudentRow = ({ student }) => {
   const studentInfo = Object.keys(student);
 
   //calculates the average score for each student following a formula
-  // const calculateScore = (score) => {
-  //   const averageScore =
-  //     (score.firstTermScore +
-  //       (score.secondTermScore + score.thirdTermScore) * 2) /
-  //     5;
-  //   return averageScore;
-  // };
   const studentScores = student.score[0];
   const studentAverageScore =
     (studentScores.firstTermScore +
       (studentScores.secondTermScore + studentScores.thirdTermScore) * 2) /
     5;
+
+  //checks if the students passes or not to color the row accordingly
+  const rowStyle = () => {
+    if (studentAverageScore >= 10) return "table-infos";
+    else return "table-dangers";
+  };
   return (
-    <tr>
+    <tr className={rowStyle()}>
       {studentInfo.map((info) => {
-        if (info !== "score") {
-          return <td key={student[info]}>{student[info]}</td>;
-        } else {
+        if (info === "score") {
           return <td key={studentAverageScore}>{studentAverageScore}</td>;
+        } else {
+          return <td key={student[info]}>{student[info]}</td>;
         }
       })}
     </tr>
