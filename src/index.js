b2c2c7f15ad3fa02ac351252a165c4e437dd4cab
Youssef04544 <youssefbenyedder04@gmail.com>
2022-01-04 10:06:09 +0100

    Injected React-Query in the project,rebuilding it just after this to redeploy on netlify and prepared it for branching to make it CRUD

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const client = new QueryClient();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <QueryClientProvider client={client}>
     <App />
-  </React.StrictMode>,
+    <ReactQueryDevtools />
+  </QueryClientProvider>,
   document.getElementById("root")
 );
